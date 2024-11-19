d812820c430d704358d586c24b969b1b74c95215
Wyatt Woker <35244096+wjwoker@users.noreply.github.com>
2024-11-19 13:44:00 -0500

    Add files via upload

--- oews_garch_models.py
+++ oews_garch_models.py
@@ -1,5 +1,7 @@
+import numpy as np
 import pandas as pd
 from arch import arch_model
+from sklearn.metrics import mean_squared_error, r2_score, precision_score, recall_score, f1_score
 
 processed_data = pd.read_csv("revised_processed_data.csv")
 
@@ -10,6 +12,9 @@ predictions = []
 for naics_code in naics_codes:
     data_naics = processed_data[processed_data["NAICS"] == naics_code]
     data_naics = data_naics.drop("NAICS", axis=1)
+    actual_2018_2019 = data_naics[data_naics["YEAR"] >= 2018]
+    actual_2018_2019 = actual_2018_2019[actual_2018_2019["YEAR"] <= 2019]
+    actual_2018_2019 = actual_2018_2019.set_index(actual_2018_2019["YEAR"])
     data_naics = data_naics[data_naics["YEAR"] < 2018]
     data_naics = data_naics.set_index(data_naics["YEAR"])
     data_naics = data_naics.drop("YEAR", axis=1)
@@ -31,10 +36,67 @@ for naics_code in naics_codes:
     variance_2018 = forecasts.variance.iloc[0].iloc[0]
     variance_2019 = forecasts.variance.iloc[0].iloc[1]
 
-    predictions.append((naics_code, (float(tot_emp_naics_2018), float(variance_2018)), (float(tot_emp_naics_2019), float(variance_2019))))
+    # 1 for increase, 0 for decrease
+    pred_inc_or_dec_2018 = 1
+    actual_inc_or_dec_2018 = 1
+    pred_inc_or_dec_2019 = 1
+    actual_inc_or_dec_2019 = 1
+
+    if (forecasts.mean.iloc[0].iloc[0]) < 0:
+        pred_inc_or_dec_2018 = 0
+    
+    if (actual_2018_2019["TOT_EMP"][2018] - data_naics_total_emp[2017] < 0):
+        actual_inc_or_dec_2018 = 0
+
+    if (forecasts.mean.iloc[0].iloc[1]) < 0:
+        pred_inc_or_dec_2019 = 0
+    
+    if (actual_2018_2019["TOT_EMP"][2019] - actual_2018_2019["TOT_EMP"][2018] < 0):
+        actual_inc_or_dec_2019 = 0
+
+    predictions.append((naics_code,
+                        (float(tot_emp_naics_2018), float(variance_2018), int(actual_2018_2019["TOT_EMP"][2018]), pred_inc_or_dec_2018, actual_inc_or_dec_2018),
+                        (float(tot_emp_naics_2019), float(variance_2019), int(actual_2018_2019["TOT_EMP"][2019]), pred_inc_or_dec_2019, actual_inc_or_dec_2019)))
 
 
 # Predictions format for each NAICS code:
-# (naics_code, (total employment prediction 2018, % variance for 2018 prediction), (total employment prediction 2019, % variance for 2019 prediction))
+# (naics_code, 
+# (total employment prediction 2018, % variance for 2018 prediction, actual total employment 2018, predicted whether increased/decreased, actual whether increased/decreased),
+# (total employment prediction 2019, % variance for 2019 prediction, actual total employment 2019, predicted whether increased/decreased, actual whether increased/decreased))
+actual = pd.Series()
+predicted = pd.Series()
+actual_inc_or_dec = pd.Series()
+pred_inc_or_dec = pd.Series()
+
 for prediction in predictions:
     print(prediction)
+    actual = np.append(actual, prediction[1][2])
+    predicted = np.append(predicted, prediction[1][0])
+    actual_inc_or_dec = np.append(actual_inc_or_dec, prediction[1][4])
+    pred_inc_or_dec = np.append(pred_inc_or_dec, prediction[1][3])
+    actual = np.append(actual, prediction[2][2])
+    predicted = np.append(predicted, prediction[2][0])
+    actual_inc_or_dec = np.append(actual_inc_or_dec, prediction[2][4])
+    pred_inc_or_dec = np.append(pred_inc_or_dec, prediction[2][3])
+
+
+print("------------------------")
+print()
+mse = mean_squared_error(actual, predicted)
+rmse = np.sqrt(mse)
+actual_min, actual_max = actual.min(), actual.max()
+normalized_rmse = rmse / (actual_max - actual_min)
+print("Normalized RMSE:", normalized_rmse)
+
+r2 = r2_score(actual, predicted)
+print("r2:", r2)
+
+actual_inc_or_dec = actual_inc_or_dec.astype(int)
+pred_inc_or_dec = pred_inc_or_dec.astype(int)
+precision = precision_score(actual_inc_or_dec, pred_inc_or_dec)
+recall = recall_score(actual_inc_or_dec, pred_inc_or_dec)
+f1 = f1_score(actual_inc_or_dec, pred_inc_or_dec)
+
+print("Precision:", precision)
+print("Recall:", recall)
+print("F1:", f1)
